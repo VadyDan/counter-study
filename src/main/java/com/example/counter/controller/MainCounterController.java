cd7c1b9d8fcd4ee03d0846a4ad01b2ebdf50dc47
VadyDan <Vadyarski@yandex.ru>
2021-09-14 13:55:58 +0500

    Stable 1 (Rest API)

---
+++ src/main/java/com/example/counter/controller/MainCounterController.java
@@ -0,0 +1,39 @@
+package com.example.counter.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/counter")
+public class MainCounterController {
+
+    private int count = 0;
+    private final String sharedKeyUp = "SHARED_KEY_UP";
+    private final String sharedKeyDown = "SHARED_KEY_DOWN";
+
+    private static final String SUCCESS_STATUS = "success";
+    private static final String ERROR_STATUS = "error";
+    private static final int CODE_SUCCESS = 100;
+    private static final int AUTH_FAILURE = 102;
+
+    @GetMapping
+    public int getCount() {
+        return count;
+    }
+
+    @PostMapping()
+    public BaseResponse pay(@RequestParam(value = "key") String key) {
+
+        final BaseResponse response;
+
+        if (sharedKeyUp.equalsIgnoreCase(key)) {
+            count++;
+            response = new BaseResponse(SUCCESS_STATUS, CODE_SUCCESS);
+        } else if (sharedKeyDown.equalsIgnoreCase(key)) {
+            count--;
+            response = new BaseResponse(SUCCESS_STATUS, CODE_SUCCESS);
+        } else {
+            response = new BaseResponse(ERROR_STATUS, AUTH_FAILURE);
+        }
+        return response;
+    }
+}
