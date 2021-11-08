3c533133cf8d9f18046d648d0f8ff343a5ab5e86
VadyDan <Vadyarski@yandex.ru>
2021-11-09 02:15:43 +0500

    Kafka Consumer Producer

--- src/main/java/com/example/counter/CounterApplication.java
+++ src/main/java/com/example/counter/CounterApplication.java
@@ -1,9 +1,21 @@
 package com.example.counter;
 
+import com.example.counter.kafka.ConsumerCreator;
+import com.example.counter.kafka.IKafkaConstants;
+import com.example.counter.kafka.ProducerCreator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.concurrent.ExecutionException;
+
 
 @SpringBootApplication
 public class CounterApplication {
@@ -13,6 +25,63 @@ public class CounterApplication {
     public static void main(String[] args) {
         SpringApplication.run(CounterApplication.class, args);
         LOG.info("Springboot application is started successfully.");
+        runProducer();
+        runConsumer();
+    }
+
+    static void runConsumer() {
+        Consumer<Long, String> consumer = ConsumerCreator.createConsumer();
+
+        int noMessageFound = 0;
+
+        while (true) {
+            ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+            // 1000 is the time in milliseconds consumer will wait if no record is found at broker.
+            if (consumerRecords.count() == 0) {
+                noMessageFound++;
+                if (noMessageFound > IKafkaConstants.MAX_NO_MESSAGE_FOUND_COUNT)
+                    // If no message found count is reached to threshold exit loop.
+                    break;
+                else
+                    continue;
+            }
+
+            //print each record.
+            consumerRecords.forEach(record -> {
+                System.out.println("Record Key " + record.key());
+                System.out.println("Record value " + record.value());
+                System.out.println("Record partition " + record.partition());
+                System.out.println("Record offset " + record.offset());
+            });
+
+            // commits the offset of record to broker.
+            consumer.commitAsync();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        consumer.close();
+    }
+
+    static void runProducer() {
+        Producer<Long, String> producer = ProducerCreator.createProducer();
+
+        for (int index = 0; index < IKafkaConstants.MESSAGE_COUNT; index++) {
+            ProducerRecord<Long, String> record = new ProducerRecord<>(IKafkaConstants.TOPIC_NAME,
+                    "This is record " + index);
+            try {
+                RecordMetadata metadata = producer.send(record).get();
+                System.out.println("Record sent with key " + index + " to partition " + metadata.partition()
+                        + " with offset " + metadata.offset());
+                Thread.sleep(1000);
+            }
+            catch (ExecutionException | InterruptedException e) {
+                System.out.println("Error in sending record");
+                System.out.println(e);
+            }
+        }
     }
 
 }
---
+++ src/main/java/com/example/counter/kafka/IKafkaConstants.java
@@ -0,0 +1,21 @@
+package com.example.counter.kafka;
+
+public interface IKafkaConstants {
+    String KAFKA_BROKERS = "localhost:9092";
+
+    Integer MESSAGE_COUNT=20;
+
+    String CLIENT_ID="client1";
+
+    String TOPIC_NAME="test";
+
+    String GROUP_ID_CONFIG="consumerGroup1";
+
+    Integer MAX_NO_MESSAGE_FOUND_COUNT=100;
+
+    String OFFSET_RESET_LATEST="latest";
+
+    String OFFSET_RESET_EARLIER="earliest";
+
+    Integer MAX_POLL_RECORDS=1;
+}
